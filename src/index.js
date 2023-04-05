4eafa22c877dd45ab8b6a680262ceaaaa15ba695
Rodrigo Bossini <professorbossini@gmail.com>
2023-04-04 21:29:51 -0300

    apostila04(props): fim da secao 2.5
    - tres pedidos implementados
    - retomar fazendo a refatoracao do codigo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className="container border rounded mt-2">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="display-5 text-center">Seus pedidos</h1>
+        </div>
+      </div>
+      <div className="row">
+        {/* mobile first */}
+        {/* .col-12.col-lg-6.col-xl-4 */}
+        <div className="col-12 col-lg-6 col-xxl-4 my-2">
+          {/* .card>.card-header.text-muted{22/04/2022} */}
+          <div className="card">
+            <div className="card-header text-muted">
+              22/04/2022
+            </div>
+            <div className="card-body d-flex">
+              
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-shake fa-hard-drive fa-2x"></i>
+              </div>
+              
+              <div className="flex-grow-1 ms-2 border">
+                <h4 className="text-center">SSD</h4>
+                <p className="text-center">SSD A400</p>
+              </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            </div>
+          </div>
+
+        </div>
+        <div className="col-12 col-lg-6 col-xxl-4 my-2">
+          <div className="card">
+            <div className="card-header text-muted">
+              20/03/2023
+            </div>
+            <div className="card-body d-flex">
+              
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-shake fa-book fa-2x"></i>
+              </div>
+              
+              <div className="flex-grow-1 ms-2 border">
+                <h4 className="text-center">Livro</h4>
+                <p className="text-center">Concrete Mathematics - Donald Knuth</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-lg-6 col-xxl-4 my-2">
+        <div className="card">
+            <div className="card-header text-muted">
+              13/04/2022
+            </div>
+            <div className="card-body d-flex">
+              
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-shake fa-laptop fa-2x"></i>
+              </div>
+              
+              <div className="flex-grow-1 ms-2 border">
+                <h4 className="text-center">Notebook</h4>
+                <p className="text-center">Notebook Dell i7 16Gb</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
